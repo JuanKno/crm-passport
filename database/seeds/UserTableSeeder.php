191e27264addf97a291144a067a02ecae75d60df
Juan Esteban <juanes0202@gmail.com>
2021-04-24 14:50:29 -0500

    creando autenticacion y migraciones para los usuarios y sus roles

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+
+        User::create([
+            'name' => 'Juan Esteban',
+            'email' => 'juan@mail.com',
+            'password' => bcrypt('secret'),
+            'role' => 'admin'
+        ]);
+    }
+}
